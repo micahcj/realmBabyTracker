c32df7644f2c05ff029db6789e7cc6c40aff879e
Micah J <micahcj@gmail.com>
2023-06-30 20:14:48 -0400

    Initial Commit

---
+++ realmBabyTracker/realmBabyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  realmBabyTrackerApp.swift
+//  realmBabyTracker
+//
+//  Created by Micah Johsnon on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct realmBabyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ realmBabyTrackerUITests/realmBabyTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  realmBabyTrackerUITestsLaunchTests.swift
+//  realmBabyTrackerUITests
+//
+//  Created by Micah Johsnon on 6/30/23.
+//
+
+import XCTest
+
+final class realmBabyTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
