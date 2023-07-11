72bd8965cb1f499344b21e5f1cadb296f38a7483
Micah J <micahcj@gmail.com>
2023-07-11 15:45:50 -0500

    Broke some stuff.

--- realmBabyTracker/ContentView.swift
+++ realmBabyTracker/ContentView.swift
@@ -15,13 +15,24 @@ import RealmSwift
 let app = App(id: "babytracker-fzeej")
 let ownerId = "123"
 
+func dateFormatter() -> String {
+    let formatter = DateFormatter()
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.dateFormat = "yyyy-MM-dd HH:mm:ss ZZZZ"
+//    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+    var dateString:String = "\(String(describing: formatter.date(from: "\(Date())")))"
+    dateString = formatter.string(from:Date())
+    return dateString
+}
+
 class PurchaseClass: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
-    @Persisted var date: String = ""
+//    @Persisted var date: String = "\(Date())"
+    @Persisted var date: String = "\(Date())"
     @Persisted var size: String = ""
     @Persisted var cost: String = ""
     @Persisted var ownerID: String
-    convenience init(ownerID : String) {
+    convenience init(size : String, cost: String) {
         self.init()
         self.ownerID = ownerID
     }
@@ -41,7 +52,29 @@ func loginAsync() async -> User {
     
 }
 
-    
+//func realmObeserver(option: String, object : realmo) {
+//    if option == "open" {
+//        let notificationToken = object.observe { (changes) in
+//            switch changes {
+//            case .initial: break
+//                // Results are now populated and can be accessed without blocking the UI
+//            case .update(_, let deletions, let insertions, let modifications):
+//                // Query results have changed.
+//                print("Deleted indices: ", deletions)
+//                print("Inserted indices: ", insertions)
+//                print("Modified modifications: ", modifications)
+//            case .error(let error):
+//                // An error occurred while opening the Realm file on the background worker thread
+//                fatalError("\(error)")
+//            }
+//        }
+//
+//    } else if option == "close" {
+//
+//    }
+//}
+
+
 @MainActor
     func openPurchaseRealm() async {
         
@@ -77,12 +110,71 @@ func login() -> User {
 @MainActor
 func useRealm(_ realm: Realm, _ user: User) {
     // Add some tasks
-    let todo = Todo(name: "Do laundry", ownerId: user.id,dateString: "\(Date())")
+    print("useRealm0")
+//    let todo = Todo(name: "Do laundry", ownerId: user.id,dateString: "\(Date())")
+//    let purchase = PurchaseClass(size: "big", cost: "a lot")
+    let purchase = Shopping(item: "sleep", size: "any", cost: "everything", ownerId: "Big MJ")
+//    print("Realm is located at:", realm.configuration.fileURL!)
     try! realm.write {
-        realm.add(todo)
+//        realm.add(todo)
+        realm.add(purchase)
     }
+//    let todos = realm.objects(Todo.self)
+    let purchases = realm.objects(Shopping.self)
+    print(purchases)
+    let notificationToken = purchases.observe { (changes) in
+        switch changes {
+        case .initial:
+            print("Initial")
+            break
+            // Results are now populated and can be accessed without blocking the UI
+        case .update(_, let deletions, let insertions, let modifications):
+            // Query results have changed.
+            print("Deleted indices: ", deletions)
+            print("Inserted indices: ", insertions)
+            print("Modified modifications: ", modifications)
+        case .error(let error):
+            // An error occurred while opening the Realm file on the background worker thread
+            fatalError("\(error)")
+        }
+    }
+    notificationToken.invalidate()
 }
 
+
+@MainActor
+func useRealm1(_ realm: Realm, _ user: User) {
+    // Add some tasks
+    print("useRealm1")
+    let todo = Todo(name: "Do laundry", ownerId: user.id,dateString: "\(Date())")
+//    let purchase = PurchaseClass(size: "big", cost: "a lot")
+//    let purchase = Shopping(item: "sleep", size: "any", cost: "everything", ownerId: "Big MJ")
+//    print("Realm is located at:", realm.configuration.fileURL!)
+    try! realm.write {
+        realm.add(todo)
+//        realm.add(purchase)
+    }
+    let todos = realm.objects(Todo.self)
+//    let purchases = realm.objects(Shopping.self)
+    print(todos)
+    let notificationToken = todos.observe { (changes) in
+        switch changes {
+        case .initial:
+            print("Initial")
+            break
+            // Results are now populated and can be accessed without blocking the UI
+        case .update(_, let deletions, let insertions, let modifications):
+            // Query results have changed.
+            print("Deleted indices: ", deletions)
+            print("Inserted indices: ", insertions)
+            print("Modified modifications: ", modifications)
+        case .error(let error):
+            // An error occurred while opening the Realm file on the background worker thread
+            fatalError("\(error)")
+        }
+    }
+    notificationToken.invalidate()
+}
 //class Task: Object {
 //    @Persisted(primaryKey: true) var _id: ObjectId
 //    @Persisted var taskName: String
@@ -114,32 +206,69 @@ func useRealm(_ realm: Realm, _ user: User) {
 @MainActor
 func writeToLocalRealm(name:String) {
     let todo = Todo(name: name, ownerId: "String", dateString: "\(Date())")
+    let shops = Shopping(item: "food", size: "lot", cost: "time", ownerId: "Big MJ")
     let realm = try! Realm()
     let todos = realm.objects(Todo.self)
-    print(todos)
-//    try! realm.write {
-//        realm.add(todo)
-//    }
+    print("write to local",todos)
+    try! realm.write {
+        realm.add(todo)
+        realm.add(shops)
+        //    }
+    }
+    print("wrote to local")
 }
 
+
+func handleClientReset() {
+    // Report the client reset error to the user, or do some custom logic.
+    print("!! -- automatic client reset FAILED -- !!")
+}
 
 @MainActor
 func openSyncedRealm(user: User) async {
     print("openSyncedRealm")
     do {
-        var config = user.flexibleSyncConfiguration()
+        var config = user.flexibleSyncConfiguration(clientResetMode: .discardUnsyncedChanges())
+//        var config = Realm.Configuration.defaultConfiguration
         config.objectTypes = [Todo.self]
-        print("config")
-        let realm = try await Realm(configuration: config, downloadBeforeOpen: .always)
+        app.syncManager.errorHandler = { error, session in
+                guard let syncError = error as? SyncError else {
+                    fatalError("Unexpected error type passed to sync error handler! \(error)")
+                }
+                switch syncError.code {
+                case .clientResetError:
+                    if let (path, clientResetToken) = syncError.clientResetInfo() {
+                        handleClientReset()
+                        SyncSession.immediatelyHandleError(clientResetToken, syncManager: app.syncManager)
+                    }
+                default:
+                    // Handle other errors...
+                    ()
+                }
+            }
+//        config.syncConfiguration?.user.flexibleSyncConfiguration()
+//        config.objectTypes = [Todo.self]
+//        print("openSync0: config","\(config.objectTypes)")
+        let realm = try await Realm(configuration: config)
         print("realm")
         let subscriptions = realm.subscriptions
-        print("subscriptions")
+        print("subscriptions - open 'realm'")
+        
         try await subscriptions.update {
-            subscriptions.append(QuerySubscription<Todo> {
+            subscriptions.append(
+                QuerySubscription<Todo> {
                 $0.ownerId == user.id
-            })
-        }
-        await useRealm(realm, user)
+            })}
+        useRealm1(realm, user)
+        print("todo")
+//        try await subscriptions.update {
+//            subscriptions.append(QuerySubscription<Shopping> {
+//                $0.ownerId == user.id
+//            })
+//        }
+        print("shopping")
+        try await useRealm1(realm, user)
+        print("used realm")
     } catch {
         print("Error opening realm: \(error.localizedDescription)")
     }
@@ -151,33 +280,38 @@ func openSyncedRealm(user: User) async {
 func openSyncedRealm1(user: User) async {
     print("opened openSyncedRealm")
     do {
-//        var config = user.flexibleSyncConfiguration()
-        var config = user.flexibleSyncConfiguration(initialSubscriptions: { subs in subs.append(
-                        QuerySubscription<Todo> {
-                            $0.ownerId == user.id
-                        })
-                })
-        print("configged")
+        var config = user.flexibleSyncConfiguration(clientResetMode: .discardUnsyncedChanges())
+//        var config = user.flexibleSyncConfiguration(initialSubscriptions: { subs in subs.append(
+//                        QuerySubscription<Shopping> {
+//                            $0.ownerId == user.id
+//                        })
+//                })
+//        config.objectTypes = [Todo.self]
+        
         // Pass object types to the Flexible Sync configuration
         // as a temporary workaround for not being able to add a
         // complete schema for a Flexible Sync app.
-        config.objectTypes = [Todo.self]
-        print("todoself")
-        let realm = try await Realm(configuration: config, downloadBeforeOpen: .always)
+        config.objectTypes = [Shopping.self,Todo.self]
+        
+        print("configged opensync1 - \(config.objectTypes)")
+        print("shopping.self")
+        let realm = try await Realm(configuration: config)
         // You must add at least one subscription to read and write from a Flexible Sync realm
         print("remote config")
-        useRealm(realm, user)
+//        writeToLocalRealm(name: "BigMJ")
+//        useRealm(realm, user)
         let subscriptions = realm.subscriptions
-        print("did something with subscriptions")
+        print("did something with subscriptions for 'realm1'")
         try await subscriptions.update {
             subscriptions.append(
-                QuerySubscription<Todo> {
+                QuerySubscription<Shopping> {
                     $0.ownerId == user.id
                 })
         }
         //        let useRealm = try! Realm(realm: realm, user: user)
         
         useRealm(realm, user)
+        print("usedrealm")
         
         //        await useRealm
     } catch {
@@ -187,14 +321,32 @@ func openSyncedRealm1(user: User) async {
 
 }
 
-    
+ 
+class Shopping: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var item: String = "null"
+    @Persisted var cost: String = ""
+    @Persisted var size: String = ""
+//    @Persisted var dateString: String = "\(Date())"
+    @Persisted var dateString: String = dateFormatter()
+    @Persisted var ownerId: String = ""
+    convenience init(item: String, size: String, cost: String, ownerId: String) {
+        self.init()
+        self.item = item
+        self.ownerId = ownerId
+        self.cost = cost
+        self.size = size
+//       self.dateString = dateString
+   }
+}
+
 
 class Todo: Object {
    @Persisted(primaryKey: true) var _id: ObjectId
    @Persisted var name: String = ""
    @Persisted var status: String = ""
    @Persisted var dateString: String = ""
-   @Persisted var ownerId: String
+   @Persisted var ownerId: String = ""
     convenience init(name: String, ownerId: String, dateString: String) {
        self.init()
        self.name = name
@@ -516,29 +668,75 @@ struct ContentView: View {
                     
                 }
             }
-            Button("\(stringButton)") {
+            Button("1 - ToDo. \(stringButton)") {
                 clicked.toggle()
 //                Text("\(realmTime())")
 //                print("login result",login())
-//                print(realmTime())
+//                print(realmTime())ƒ
                 Task {
                     let user1 = try await app.login(credentials: Credentials.anonymous)
 //                    writeToLocalRealm(name: "Shiddddddddddddddd")
                     stringButton = "\(user1)"
                     print("logged in and made user",user1)
                     do {
-                        let realm = try await Realm()
-                        print("we reaLmed") }
-                    catch {
-                        print("realm ain't realming")
+                        ////                        let realm = try await Realm()
+                        //                        let rerealm = try await openSyncedRealm1(user: user1)
+                        try await openSyncedRealm(user: user1)
+                        print("1.. we reaLmed") //}
+                    } catch {
+                        print("2.. realm ain't realming")
                     }
-                    writeToLocalRealm(name: "maneShid")
-                    try! await openSyncedRealm(user: user1)
+//                    writeToLocalRealm(name: "maneShid")
+//                    try! await openSyncedRealm(user: user1)
+                    
                     
 //                    var syncUser : User?
                     
                     print("opened synced realm")
                 }
+//                Task {
+//                    let user1 = try await app.login(credentials: Credentials.anonymous)
+//                    do {
+//                        try await openSyncedRealm(user: user1)
+//                    } catch {
+//                        print("we couldn't openSyncedRealm")
+//                    }
+//                }
+            }
+            Button("2 - Shopping. \(stringButton)") {
+                clicked.toggle()
+//                Text("\(realmTime())")
+//                print("login result",login())
+//                print(realmTime())ƒ
+                Task {
+                    let user1 = try await app.login(credentials: Credentials.anonymous)
+//                    writeToLocalRealm(name: "Shiddddddddddddddd")
+                    stringButton = "\(user1)"
+                    print("logged in and made user",user1)
+                    do {
+                        ////                        let realm = try await Realm()
+                        //                        let rerealm = try await openSyncedRealm1(user: user1)
+                        try await openSyncedRealm1(user: user1)
+                        print("1.. we reaLmed") //}
+                    } catch {
+                        print("2.. realm ain't realming")
+                    }
+//                    writeToLocalRealm(name: "maneShid")
+//                    try! await openSyncedRealm(user: user1)
+                    
+                    
+//                    var syncUser : User?
+                    
+                    print("opened synced realm")
+                }
+//                Task {
+//                    let user1 = try await app.login(credentials: Credentials.anonymous)
+//                    do {
+//                        try await openSyncedRealm(user: user1)
+//                    } catch {
+//                        print("we couldn't openSyncedRealm")
+//                    }
+//                }
             }
             Button("Diapers") {
                 //                for var item in [showPurchases,showFeedings,showDiapers] {
