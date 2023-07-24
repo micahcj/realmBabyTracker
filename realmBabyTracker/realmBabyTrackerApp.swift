bfa567c88b6a9a0878a71b72970667bcdc5cb25d
Micah J <micahcj@gmail.com>
2023-07-24 00:29:42 -0500

    bruh. so fucking much work went into this shit. and like so many other breakthroughs... I was being dumb. Variable scope fucked me again. I have learned to look at the color coding

--- realmBabyTracker/realmBabyTrackerApp.swift
+++ realmBabyTracker/realmBabyTrackerApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct realmBabyTrackerApp: App {
+    var network = Network()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(network)
         }
     }
 }
